c82a6ac38cfd24984a5018a245f362dde1dfbcfd
ifret <129610806+ifret@users.noreply.github.com>
2024-07-14 11:28:23 +0800

    Update _worker.js
    1

--- _worker.js
+++ _worker.js
@@ -7,11 +7,11 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'cb3f8261-f3d9-492b-9fa2-44158bb67c7c';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
-const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
+const proxyIPs= ['fbi.gov', 'www.baipiao.eu.org', 'www.sean-now.com', 'www.gov.ua', 'download.yunzhongzhuan.com', 'www.udemy.com'];
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
